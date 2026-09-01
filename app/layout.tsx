86ca590644937f4f935be020fefb3d901ff85624
yaroslavnapril <prilepskaaaroslavna@gmail.com>
2026-09-01 17:05:06 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
-      <body className="antialiased">{children}</body>
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
     </html>
   )
 }
