c8c7edcecbd5916fb1bec09a16e47f6a6d93acbc
yaroslavnapril <prilepskaaaroslavna@gmail.com>
2026-09-20 18:31:42 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,13 @@ export const metadata: Metadata = {
   description: 'Платформа для учеников и репетиторов',
 };
 
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({
   children,
 }: {
